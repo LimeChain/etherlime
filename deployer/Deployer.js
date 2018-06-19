0ab3ef429e2554f1f5d49155bd7bf6d3607106cb
George Spasov <spasov05203@gmail.com>
2018-06-19 20:49:23 +0300

    Making the code easily extensible and documented

--- deployer/Deployer.js
+++ deployer/Deployer.js
@@ -3,6 +3,15 @@ const colors = require('../utils/colors');
 const Wallet = ethers.Wallet;
 
 class Deployer {
+
+	/**
+	 * 
+	 * Instantiates new deployer. You probably should not use this class directly but use something inheriting this
+	 * 
+	 * @param {*} wallet ethers.Wallet instance
+	 * @param {*} provider ethers.provider instance
+	 * @param {*} defaultOverrides [Optional] default deployment overrides
+	 */
 	constructor(wallet, provider, defaultOverrides) {
 		if (!(wallet instanceof Wallet)) {
 			throw new Error('Passed wallet is not instance of ethers Wallet');
@@ -15,40 +24,68 @@ class Deployer {
 		this.defaultOverrides = defaultOverrides;
 	}
 
-	toString() {
-		return this.network + this.wallet;
-	}
-
+	/**
+	 * 
+	 * Use this function to deploy a contract.
+	 * 
+	 * @return DeploymentResult object
+	 * 
+	 * @param {*} contract the contract object to be deployed. Must have at least abi and bytecode fields. For now use the .json file generated from truffle compile
+	 */
 	async deploy(contract) {
 
 		const deploymentArguments = Array.prototype.slice.call(arguments);
 		deploymentArguments.splice(0, 1);
 
-		if (deploymentArguments.length > 0) {
-			console.log(`Deploying contract: ${colors.colorName(contract.contractName)} with arguments: ${colors.colorParams(deploymentArguments)}`);
-		} else {
-			console.log(`Deploying contract: ${colors.colorName(contract.contractName)}`);
-		}
+		this._preValidateArguments(contract, deploymentArguments);
 
-		const deployTransaction = ethers.Contract.getDeployTransaction(contract.bytecode, contract.abi, ...deploymentArguments);
+		const deployTransaction = this._prepareDeployTransaction(contract, deploymentArguments);
 
-		const transaction = await this.wallet.sendTransaction(this._overrideDeployTransactionConfig(deployTransaction));
+		const transaction = await this._sendDeployTransaction(this._overrideDeployTransactionConfig(deployTransaction));
 
-		console.log(`Waiting for transaction to be included in block and mined: ${colors.colorTransactionHash(transaction.hash)}`);
-		await this.provider.waitForTransaction(transaction.hash);
+		await this._waitForDeployTransaction(transaction);
 
-		const receipt = await this.provider.getTransactionReceipt(transaction.hash);
+		const transactionReceipt = await this._getTransactionReceipt(transaction);
 
-		if (receipt.status === 0) {
-			throw new Error(`Transaction ${colors.colorTransactionHash(receipt.transactionHash)} ${colors.colorFailure('failed')}. Please check etherscan for better reason explanation.`);
-		}
+		this._postValidateTransaction(contract, transaction, transactionReceipt)
 
-		console.log(`Contract ${colors.colorName(contract.contractName)} deployed at address: ${colors.colorAddress(receipt.contractAddress)}`);
+		const deploymentResult = this._generateDeploymentResult(contract, transaction, transactionReceipt);
 
-		return receipt.contractAddress;
+		return deploymentResult;
 
 	}
 
+	/**
+	 * 
+	 * Override for custom pre-send validation
+	 *
+	 * @param {*} contract the contract to be deployed
+	 * @param {*} deploymentArguments the deployment arguments
+	 */
+	_preValidateArguments(contract, deploymentArguments) {
+		const deployContractStart = `\nDeploying contract: ${colors.colorName(contract.contractName)}`;
+		const argumentsEnd = (deploymentArguments.length == 0) ? '' : ` with arguments: ${colors.colorParams(deploymentArguments)}`;
+
+		console.log(`${deployContractStart}${argumentsEnd}`);
+	}
+
+	/**
+	 * 
+	 * Override this to include custom logic for deploy transaction generation
+	 * 
+	 * @param {*} contract the contract to be deployed
+ 	 * @param {*} deploymentArguments the arguments to this contract
+	 */
+	_prepareDeployTransaction(contract, deploymentArguments) {
+		return ethers.Contract.getDeployTransaction(contract.bytecode, contract.abi, ...deploymentArguments);
+	}
+
+	/**
+	 * 
+	 * Override this for custom deploy transaction configuration
+	 * 
+	 * @param {*} deployTransaction the transaction that is to be overridden
+	 */
 	_overrideDeployTransactionConfig(deployTransaction) {
 		if (this.defaultOverrides === undefined) {
 			return deployTransaction;
@@ -63,7 +100,65 @@ class Deployer {
 		}
 
 		return deployTransaction;
+
 	}
+
+	/**
+	 * 
+	 * Override this to include custom logic for sending the deploy transaction
+	 * 
+	 * @param {*} deployTransaction the transaction that is to be sent
+	 */
+	_sendDeployTransaction(deployTransaction) {
+		return this.wallet.sendTransaction(deployTransaction);
+	}
+
+	/**
+	 * 
+	 * Override this to include custom logic for waiting for deployed transaction. For example you could trigger mined block for testrpc/ganache-cli
+	 * 
+	 * @param {*} transaction The sent transaction object to be waited for
+	 */
+	async _waitForDeployTransaction(transaction) {
+		console.log(`Waiting for transaction to be included in block and mined: ${colors.colorTransactionHash(transaction.hash)}`);
+		await this.provider.waitForTransaction(transaction.hash);
+	}
+
+	/**
+	 * 
+	 * Override this to include custom receipt getting logic
+	 * 
+	 * @param {*} transaction the already mined transaction
+	 */
+	_getTransactionReceipt(transaction) {
+		return this.provider.getTransactionReceipt(transaction.hash);
+	}
+
+	/**
+	 * 
+	 * @param {*} contract the contract being deployed
+	 * @param {*} transaction the transaction object being sent
+	 * @param {*} transactionReceipt the transaction receipt
+	 */
+	_postValidateTransaction(contract, transaction, transactionReceipt) {
+		if (transactionReceipt.status === 0) {
+			throw new Error(`Transaction ${colors.colorTransactionHash(transactionReceipt.transactionHash)} ${colors.colorFailure('failed')}. Please check etherscan for better reason explanation.`);
+		}
+	}
+
+	/**
+	 * 
+	 * Override this for custom deployment result objects
+	 * 
+	 * @param {*} contract the contract that has been deployed
+	 * @param {*} transaction the transaction object that was sent
+	 * @param {*} transactionReceipt the transaction receipt
+	 */
+	_generateDeploymentResult(contract, transaction, transactionReceipt) {
+		console.log(`Contract ${colors.colorName(contract.contractName)} deployed at address: ${colors.colorAddress(transactionReceipt.contractAddress)}`);
+		return transactionReceipt.contractAddress
+	}
+
 }
 
 module.exports = Deployer;
--- infuraDeployer/InfuraPrivateKeyDeployer.js
+++ infuraDeployer/InfuraPrivateKeyDeployer.js
@@ -5,15 +5,26 @@ const colors = require('../utils/colors');
 
 class InfuraPrivateKeyDeployer extends PrivateKeyDeployer {
 
+	/**
+	 * 
+	 * Instantiates new deployer based on the Infura service and private key based deployment wallet
+	 * 
+	 * @param {*} privateKey the private key for the deployer wallet
+	 * @param {*} network network to deploy on. Check ethers for all networks. Examples: mainnet, rinkeby, ropsten
+	 * @param {*} apiKey the apiKey given to you by Infura
+	 * @param {*} defaultOverrides [Optional] default deployment overrides
+	 */
 	constructor(privateKey, network, apiKey, defaultOverrides) {
 		const infuraProvider = new ethers.providers.InfuraProvider(ethers.providers.networks[network], apiKey);
 		super(privateKey, infuraProvider, defaultOverrides);
 		console.log(`Deployer set to Infura. Network: ${colors.colorNetwork(network)} with API Key: ${colors.colorAPIKey(apiKey)}\n`)
+		this.network = network;
+		this.apiKey = apiKey;
 	}
 
 	toString() {
-		const baseString = super.toString();
-		return `Infura ${baseString}`;
+		const superString = super.toString();
+		return `Deployer set to Infura. Network: ${colors.colorNetwork(this.network)} with API Key: ${colors.colorAPIKey(this.apiKey)}\n${superString}`;
 	}
 }
 
