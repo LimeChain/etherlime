d281b3d2bd339105c0fde077a1a1acfc324e3216
ochikov <ochikov@users.noreply.github.com>
2018-06-20 16:43:29 +0300

    added json rpc provider (#4)

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const PrivateKeyDeployer = require('./deployer/private-key-deployer');
 const InfuraPrivateKeyDeployer = require('./deployer/infura-deployer/infura-private-key-deployer');
+const JSONRPCPrivateKeyDeployer = require('./deployer/jsonrpc-deployer/jsonrpc-private-key-deployer');
 
 const DeployedContractWrapper = require('./deployed-contract/deployed-contract-wrapper');
 
 module.exports = {
 	PrivateKeyDeployer,
 	InfuraPrivateKeyDeployer,
+	JSONRPCPrivateKeyDeployer,
 	DeployedContractWrapper
 }
