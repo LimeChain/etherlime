b908b3a4c2f93b5d511f777beb965168df4a81e1
George Spasov <spasov05203@gmail.com>
2018-06-23 08:33:36 +0300

    Fixed desription

--- cli-commands/commands.js
+++ cli-commands/commands.js
@@ -3,7 +3,7 @@ const ganache = require('./ganache/ganache');
 const commands = [
 	{
 		command: 'ganache [port]',
-		description: 'start etherlime ganache-cli instance with static accounts with a lot of ETH. Use this with GanacheCliDeployer',
+		description: 'start etherlime ganache-cli instance with static accounts with a lot of ETH.',
 		argumentsProcessor: (yargs) => {
 			yargs.positional('port', {
 				describe: 'port to run ganache-cli on',
