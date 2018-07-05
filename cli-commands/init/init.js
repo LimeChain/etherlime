9b8cca0ff42ffab368491d07154de22d7b4f7423
George Spasov <spasov05203@gmail.com>
2018-07-05 22:12:18 +0300

    Added npm install etherlime on etherlime init

--- cli-commands/init/init.js
+++ cli-commands/init/init.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 const deploymentDir = './deployment';
 const deploymentFileDestination = `${deploymentDir}/deploy.js`;
 
 const createDeploymentDir = () => {
+	console.log('===== Creating deployment file structure =====')
 	if (!fs.existsSync(deploymentDir)) {
 		fs.mkdirSync(deploymentDir);
 	}
@@ -19,11 +22,14 @@ const copyDeployFile = (libraryDirectory) => {
 	fs.copyFileSync(deploymentFileSource, deploymentFileDestination);
 }
 
-const run = () => {
+const run = async () => {
 
 	const libraryDirectory = __dirname;
 
 	try {
+		console.log('===== Installing etherlime =====');
+		const { stdout, stderr } = await exec('npm install etherlime');
+		console.log(stdout);
 		createDeploymentDir();
 		copyDeployFile(libraryDirectory);
 		console.log(`Etherlime was successfully initialized! Check ${deploymentFileDestination} for your deployment script.`);
