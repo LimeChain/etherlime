b2a3116452bffdcc4312e684100d002c8bf3ebf5
Dimitar Dzhurenov <dimitar.dzhurenov@gmail.com>
2018-07-17 15:12:52 +0300

    small fixes

--- cli-commands/compiler/compiler.js
+++ cli-commands/compiler/compiler.js
@@ -2,9 +2,6 @@ let compiler = require("./etherlime-workflow-compile/index");
 let Resolver = require("./etherlime-resolver/index");
 
 const performCompilation = (defaultPath) => {
-    // const deploymentFile = `${process.cwd()}/${_deploymentFilePath}`;
-    defaultPath = `${process.cwd()}/${defaultPath}`;
-
     let resolverOptions = {
         "working_directory": `${defaultPath}/contracts`,
         "contracts_build_directory": `${defaultPath}/build`
@@ -17,17 +14,14 @@ const performCompilation = (defaultPath) => {
         "contracts_build_directory": `${defaultPath}/build`
     };
 
-    // console.log('compileOptions = ', compileOptions);
-    // console.log('resolverOptions = ', resolverOptions);
-
     compiler.compile(compileOptions, (error, artifacts, paths) => { 
-        // console.log('error = ', error);
-        // console.log('artifacts = ', artifacts);
-        console.log('paths = ', paths);
+        
     });
 }
 
 const run = async (defaultPath) => {
+    defaultPath = `${process.cwd()}/${defaultPath}`;
+
     performCompilation(defaultPath);
 }
 
