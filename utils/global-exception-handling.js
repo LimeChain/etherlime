84fb692949c35f28f80518845fd786a2b42e38c0
Vladdy <ivanov.vladdy@gmail.com>
2018-08-24 13:23:54 +0300

    Handle rejection and exception

--- utils/global-exception-handling.js
+++ utils/global-exception-handling.js
@@ -1,8 +1,17 @@
-const globalExceptionHandling = process.on('unhandledRejection', err => {
-	console.log('Udri Filio');
+const unhandledRejection = process.on('unhandledRejection', err => {
 	console.error((new Date).toUTCString() + ' unhandledRejection:', err.message)
 	console.error(err.stack)
 });
+
+const unhandledException = process.on('unhandledException', err => {
+	console.error((new Date).toUTCString() + ' unhandledException:', err.message)
+	console.error(err.stack)
+});
+
+const globalExceptionHandling = function () {
+	unhandledException;
+	unhandledRejection
+};
 
 module.exports = {
 	globalExceptionHandling
