5788dc560d9178aa362ba0fe34dcab4ae3352cd3
Vladdy <ivanov.vladdy@gmail.com>
2018-08-29 19:33:54 +0300

    Resolve comments

--- cli-commands/init/LimeFactory.sol
+++ cli-commands/init/LimeFactory.sol
@@ -2,21 +2,19 @@ pragma solidity ^0.4.24;
 
 contract LimeFactory {
 
-    event FreshLime(string name, uint energy);
+    event FreshLime(string name);
 
     struct Lime {
         string name;
         uint8 carbohydrates;
         uint8 fat;
         uint8 protein;
-        uint8 energy;
     }
 
     Lime[] public limes;
 
     function createLime(string _name, uint8 _carbohydrates, uint8 _fat, uint8 _protein) internal {
-        uint8 energy = _carbohydrates * 3 + _fat * 2 + _protein;
-        limes.push(Lime(_name, _carbohydrates, _fat, _protein, energy)) - 1;
-        FreshLime(_name, energy);
+        limes.push(Lime(_name, _carbohydrates, _fat, _protein));
+        FreshLime(_name);
     }
 }
