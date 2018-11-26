31147d6ba8bb597988e3412488d809bc77b68128
desimira <35794384+desimira@users.noreply.github.com>
2018-11-26 15:29:01 +0200

    Tests deploy command optimization (#112)
    * Deployment cli command tests
    
    * 100% deploy CLI command test coverage and history records fixing
    
    * optimizing tests
    
    * rename testDeploy files
    
    * test optimization
    
    * Update package.json

--- test/cli-commands/deploy/deploy.js
+++ test/cli-commands/deploy/deploy.js
@@ -7,13 +7,16 @@ const fs = require('fs-extra');
 
 const deployer = require('../../../cli-commands/deployer/deployer');
 const compiler = require('../../../cli-commands/compiler/compiler');
+const logger = require('../../../logger-service/logger-service').logger;
 const file = require('./deploymentScriptsFile').file;
 let compileSpy = sinon.spy(compiler, "run");
+let loggerSpy = sinon.spy(logger, "log")
 
 let privateKey = "7ab741b57e8d94dd7e1a29055646bafde7010f38a900f55bbd7647880faa6ee8";
 let wrongPrivateKey = "7ab741b57e8d94dd7e1a29055646bafde7010f38a900f55bbd7647880faa6ee9";
 
 const specificFile = 'test/cli-commands/deploy/deploymentScripts.js';
+const successfulMessage = "Your deployment script finished successfully!"
 const deployError = "deployment/deploy.js file not found. Probably you've not initialized etherlime. Please run etherlime init first.";
 const errorMessage = "sender doesn't have enough funds to send tx.";
 const deployFolder = './deployment';
@@ -26,6 +29,7 @@ describe('Deploy cli command', () => {
         fs.mkdirSync(deployFolder)
         fs.writeFileSync(`${deployFolder}/deploy.js`, file);
         await assert.isFulfilled(deployer.run(), 'It was not successfully executed');
+        sinon.assert.calledWithExactly(loggerSpy, successfulMessage)
         await fs.removeSync(deployFolder)
     });
 
@@ -35,14 +39,17 @@ describe('Deploy cli command', () => {
 
     it('should deploy specific file', async function () {
         await assert.isFulfilled(deployer.run(specificFile), 'It was not successfully executed');
+        sinon.assert.calledWithExactly(loggerSpy, successfulMessage)
     });
 
     it('should deploy on specific network', async function () {
         await assert.isFulfilled(deployer.run(specificFile, 'local'), 'It was not successfully executed');
+        sinon.assert.calledWithExactly(loggerSpy, successfulMessage)
     });
 
-    it('should deploy wiht secret parameter', async function () {
+    it('should deploy with secret parameter', async function () {
         await assert.isFulfilled(deployer.run(specificFile, undefined, privateKey), 'It was not successfully executed');
+        sinon.assert.calledWithExactly(loggerSpy, successfulMessage)
     });
 
     it('should throw error on deployment failure if silent is false', async function () {    
@@ -67,21 +74,26 @@ describe('Deploy cli command', () => {
         fs.mkdirSync(contractFolder)
         await deployer.run(specificFile, undefined, undefined, undefined, true);
         sinon.assert.calledWith(compileSpy, '.');
+        sinon.assert.calledWithExactly(loggerSpy, successfulMessage)
     });
 
     it('should deploy with compile and run parameters', async function () {
         let run = 99;
         await deployer.run(specificFile, undefined, undefined, undefined, true, run);
         sinon.assert.calledWithExactly(compileSpy, ".", run);
+        sinon.assert.calledWithExactly(loggerSpy, successfulMessage)
         fs.removeSync(contractFolder);
         compileSpy.restore();
     });
 
-    it('should deploy with specific otuput parameter "normal"', async function () {
+    it('should deploy with specific output parameter "normal"', async function () {
         await assert.isFulfilled(deployer.run(specificFile, undefined, undefined, undefined, undefined, undefined, 'normal'), 'It was not successfully executed')
+        sinon.assert.calledWithExactly(loggerSpy, successfulMessage)
     });
 
     afterEach(async function () {
-        await fs.removeSync('./.etherlime-store');
+        loggerSpy.restore();
+        await fs.removeSync('./.etherlime-store')
+        await fs.removeSync('./.etherlime-store/.history.json');
     })
 })
