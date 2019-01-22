5910c8209cee27b2caa7f127545099fcb59f409c
Desimira <desimira@limechain.tech>
2019-01-22 15:31:47 +0200

    additional test for covering compile code

--- test/cli-commands/compile/compile-dependencies.js
+++ test/cli-commands/compile/compile-dependencies.js
@@ -418,6 +418,26 @@ describe('Compile dependencies', () => {
             await assert.isRejected(fnExecution, expectedError)
         });
 
+        it('should throw err if there is syntax err', async function() {
+            let expectedError = "Expected ';' but got end of source"
+            const sourceObject = {
+                "::contracts\\Empty.sol": 'pragma solidity ^0.5.0 contract Empty {\n\n}'
+            }
+
+            let fnExecution = new Promise((resolve, reject) => {
+                etherlimeCompile(sourceObject, compileOptions, function (err) {
+                    if (!err) {
+                        resolve()
+                        return
+                    }
+                    reject(err)
+                });
+            });
+
+            await assert.isRejected(fnExecution, expectedError)
+
+        })
+
         it('should replace \\ with /', async function () {
             compileOptions.strict = true;
             const sourceObject = {
@@ -572,7 +592,6 @@ describe('Compile dependencies', () => {
                         resolve()
                         return
                     }
-                    console.log('hERE',err)
                     reject(err)
                 });
             });
@@ -588,7 +607,6 @@ describe('Compile dependencies', () => {
                         resolve()
                         return
                     }
-                    console.log('HERE2', err);
                     reject(err)
                 });
             });
