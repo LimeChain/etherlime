8adb0205405ab8d80267b74b653668351652fe11
BenSchZA <bscholtz.bds@gmail.com>
2019-01-23 10:01:40 +0200

    Add devnet deployer & wrapper to index

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const PrivateKeyDeployer = require('./deployer/private-key-deployer');
 const InfuraPrivateKeyDeployer = require('./deployer/infura-deployer/infura-private-key-deployer');
 const JSONRPCPrivateKeyDeployer = require('./deployer/jsonrpc-deployer/jsonrpc-private-key-deployer');
 const EtherlimeGanacheDeployer = require('./deployer/etherlime-ganache-deployer/etherlime-ganache-deployer');
+const EtherlimeDevnetDeployer = require('./deployer/etherlime-devnet-deployer/etherlime-devnet-deployer');
 
 const DeployedContractWrapper = require('./deployed-contract/deployed-contract-wrapper');
 const EtherlimeGanacheWrapper = require('./deployed-contract/etherlime-ganache-wrapper');
+const EtherlimeDevnetWrapper = require('./deployed-contract/etherlime-devnet-wrapper');
 const ContractAt = require('./deployed-contract/contract-at');
 
 module.exports = {
