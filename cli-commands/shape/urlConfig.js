946267352b3c455e5772c801c3550f213752f1b2
Ognyan Chikov <o.chikov@propy.com>
2019-02-15 09:41:09 +0200

    added react url

--- cli-commands/shape/urlConfig.js
+++ cli-commands/shape/urlConfig.js
@@ -1,5 +1,5 @@
 let repoToUrlMap = new Map();
 
-repoToUrlMap.set('angular', 'LimeChain/etherlime-shape-angular.git');
+repoToUrlMap.set('angular', 'LimeChain/etherlime-shape-angular.git', 'react', 'LimeChain/etherlime-shape-react.git');
 
-module.exports = {repoToUrlMap}
+module.exports = { repoToUrlMap }
