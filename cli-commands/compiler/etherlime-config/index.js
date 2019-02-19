ee15e0a9426f156750fa2e4c04650e036edc5639
Desimira <desimira@limechain.tech>
2019-02-19 11:28:57 +0200

    config refactoring

--- cli-commands/compiler/etherlime-config/index.js
+++ cli-commands/compiler/etherlime-config/index.js
@@ -1,132 +1,132 @@
-var _ = require("lodash");
-var path = require("path");
+const _ = require("lodash");
+const path = require("path");
 
-function Config(etherlime_directory, working_directory, network) {
-  var self = this;
 
-  var default_tx_values = {
-    gas: 6721975,
-    gasPrice: 100000000000, // 100 Shannon,
-    from: null
-  };
+/**
+	 *
+	 * @param {*} etherlime_directory path to compiler directory
+	 * @param {*} working_directory current working directory 
+	 */
 
-  this._values = {
-    etherlime_directory: etherlime_directory || path.resolve(path.join(__dirname, "../")),
-    working_directory: working_directory || process.cwd(),
-    verboseRpc: false,
-    build: null,
-    resolver: null,
-    artifactor: null,
-    solc: {
-      optimizer: {
-        enabled: false,
-        runs: 200
+class Config {
+
+  constructor (etherlime_directory, working_directory) {
+    const self = this;
+
+    let default_tx_values = {
+      gas: 6721975,
+      gasPrice: 100000000000, // 100 Shannon,
+      from: null
+    };
+  
+    this._values = {
+      etherlime_directory: etherlime_directory || path.resolve(path.join(__dirname, "../")),
+      working_directory: working_directory || process.cwd(),
+      verboseRpc: false,
+      build: null,
+      resolver: null,
+      artifactor: null,
+      solc: {
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+        evmVersion: "byzantium"
       },
-      evmVersion: "byzantium"
-    },
-    logger: {
-      log: function () { },
-    }
+      logger: {
+        log: function () { },
+      }
+    };
+
+
+    this.props = {
+  
+      etherlime_directory: function () { },
+      working_directory: function () { },
+      verboseRpc: function () { },
+      build: function () { },
+      resolver: function () { },
+      artifactor: function () { },
+      solc: function () { },
+      logger: function () { },
+  
+      build_directory: function () {
+        return path.join(self.working_directory, "build");
+      },
+      contracts_directory: function () {
+        return path.join(self.working_directory, "contracts");
+      },
+      test_directory: function () {
+        return path.join(self.working_directory, "test");
+      },
+      test_file_extension_regexp: function () {
+        return /.*\.(js|es|es6|jsx|sol)$/
+      }
+    };
+  
+    Object.keys(self.props).forEach(function (prop) {
+      self.addProp(prop, self.props[prop]);
+    });
+  }
+
+
+  addProp (key, obj) {
+    Object.defineProperty(this, key, {
+      get: obj.get || function () {
+        return this._values[key] || obj();
+      },
+      set: obj.set || function (val) {
+        this._values[key] = val;
+      },
+      configurable: true,
+      enumerable: true
+    });
+  };
+  
+  
+  normalize (obj) {
+    var clone = {};
+  
+    Object.keys(obj).forEach(function (key) {
+      try {
+        clone[key] = obj[key];
+      } catch (e) {
+  
+      }
+    });
+  
+    return clone;
+  }
+
+
+  with (obj) {
+    var normalized = this.normalize(obj);
+    var current = this.normalize(this);
+  
+    return _.extend({}, current, normalized);
   };
 
-  var props = {
 
-    etherlime_directory: function () { },
-    working_directory: function () { },
-    verboseRpc: function () { },
-    build: function () { },
-    resolver: function () { },
-    artifactor: function () { },
-    solc: function () { },
-    logger: function () { },
-
-    build_directory: function () {
-      return path.join(self.working_directory, "build");
-    },
-    contracts_directory: function () {
-      return path.join(self.working_directory, "contracts");
-    },
-    test_directory: function () {
-      return path.join(self.working_directory, "test");
-    },
-    test_file_extension_regexp: function () {
-      return /.*\.(js|es|es6|jsx|sol)$/
-    },
-    example_project_directory: function () {
-      return path.join(self.etherlime_directory, "example");
-    }
+  merge (obj) {
+    var self = this;
+    var clone = this.normalize(obj);
+  
+    Object.keys(obj).forEach(function (key) {
+      try {
+        self[key] = clone[key];
+      } catch (e) {
+  
+      }
+    });
+  
+    return this;
   };
 
-  Object.keys(props).forEach(function (prop) {
-    self.addProp(prop, props[prop]);
-  });
-};
-
-Config.prototype.addProp = function (key, obj) {
-  Object.defineProperty(this, key, {
-    get: obj.get || function () {
-      return this._values[key] || obj();
-    },
-    set: obj.set || function (val) {
-      this._values[key] = val;
-    },
-    configurable: true,
-    enumerable: true
-  });
-};
-
-Config.prototype.normalize = function (obj) {
-  var clone = {};
-
-  Object.keys(obj).forEach(function (key) {
-    try {
-      clone[key] = obj[key];
-    } catch (e) {
-
-    }
-  });
-
-  return clone;
 }
-
-Config.prototype.with = function (obj) {
-  var normalized = this.normalize(obj);
-  var current = this.normalize(this);
-
-  return _.extend({}, current, normalized);
-};
-
-Config.prototype.merge = function (obj) {
-  var self = this;
-  var clone = this.normalize(obj);
-
-  Object.keys(obj).forEach(function (key) {
-    try {
-      self[key] = clone[key];
-    } catch (e) {
-
-    }
-  });
-
-  return this;
-};
 
 Config.default = function () {
   return new Config();
 };
 
-// Config.load = function (file, options) {
-//   var config = new Config();
-
-//   config.working_directory = path.dirname(path.resolve(file));
-
-//   delete require.cache[Module._resolveFilename(file, module)];
-//   var static_config = originalRequire(file);
-
-//   config.merge(static_config);
-//   config.merge(options);
-
-//   return config;
-// };
 
 module.exports = Config;
