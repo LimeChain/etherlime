93ffd70fb2cabf5ac8dcd57add7a69fb48266579
Ognyan Chikov <o.chikov@propy.com>
2019-02-27 10:07:48 +0200

    refactor debugger implementation

--- cli-commands/compiler/etherlime-error/index.js
+++ cli-commands/compiler/etherlime-error/index.js
@@ -1,18 +1,4 @@
 var ExtendableBuiltin = require("./extendablebuiltin");
-// var inherits = require("util").inherits;
-
-// inherits(ExtendableError, ExtendableBuiltin(Error));
-
-// function ExtendableError(message) {
-//   ExtendableError.super_.call(this);
-//   this.message = message;
-//   this.stack = (new Error(message)).stack;
-//   this.name = this.constructor.name;
-// };
-
-// ExtendableError.prototype.formatForMocha = function () {
-//   this.message = this.message.replace(/\n/g, "\n     ");
-// };
 
 class ExtendableError extends ExtendableBuiltin(Error) {
   constructor(message) {
