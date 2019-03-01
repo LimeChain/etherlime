f4aded5a400a0501a774373784235ac7da8bf60d
Ognyan Chikov <o.chikov@propy.com>
2019-03-01 15:17:40 +0200

    add single file compilation support

--- cli-commands/compiler/etherlime-contract-sources/index.js
+++ cli-commands/compiler/etherlime-contract-sources/index.js
@@ -1,17 +1,27 @@
 const dir = require("node-dir");
 const path = require("path");
+const fs = require("fs");
 
-let find_contracts = async (directory) => {
+let find_contracts = async (workingPath) => {
   return new Promise((resolve, reject) => {
-    dir.files(directory, function (err, files) {
-      if (err) {
-        return reject(err);
+    let isDirExists = fs.existsSync(workingPath) && fs.lstatSync(workingPath).isDirectory();
+    if (isDirExists) {
+      dir.files(workingPath, function (err, files) {
+        if (err) {
+          return reject(err);
+        }
+        files = files.filter(function (file) {
+          return path.extname(file) == ".sol" && path.basename(file)[0] != ".";
+        });
+        return resolve(files);
+      })
+    } else {
+      if (path.extname(workingPath) == '.sol') {
+        return resolve([workingPath]);
       }
-      files = files.filter(function (file) {
-        return path.extname(file) == ".sol" && path.basename(file)[0] != ".";
-      });
-      return resolve(files);
-    })
+      return reject(new Error('Your file is not solidity file!'));
+    }
+
   });
 }
 
