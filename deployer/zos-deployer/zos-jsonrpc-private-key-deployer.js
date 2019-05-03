258134876db610f77755031e842c153269fdf8d2
Desimira <desimira@limechain.tech>
2019-05-03 14:53:53 +0300

    test fixes and code optimization

--- deployer/zos-deployer/zos-jsonrpc-private-key-deployer.js
+++ deployer/zos-deployer/zos-jsonrpc-private-key-deployer.js
@@ -1,8 +1,8 @@
-const { ZWeb3, Contracts, SimpleProject } = require('zos-lib')
-const fs = require('fs') 
-const path = require('path')
+const { ZWeb3, Contracts, SimpleProject } = require('zos-lib');
+const fs = require('fs');
+const path = require('path');
 
-const JSONRPCPrivateKeyDeployer = require('../jsonrpc-deployer/jsonrpc-private-key-deployer')
+const JSONRPCPrivateKeyDeployer = require('../jsonrpc-deployer/jsonrpc-private-key-deployer');
 
 
 class ZosJSONRPCPrivateKeyDeployer extends JSONRPCPrivateKeyDeployer {
@@ -15,100 +15,104 @@ class ZosJSONRPCPrivateKeyDeployer extends JSONRPCPrivateKeyDeployer {
 	 * @param {*} defaultOverrides [Optional] default deployment overrides
 	 */
 	constructor(privateKey, nodeUrl, defaultOverrides) {
-        super(privateKey, nodeUrl, defaultOverrides)
-        ZWeb3.initialize(this.nodeUrl)
+        super(privateKey, nodeUrl, defaultOverrides);
+        ZWeb3.initialize(this.nodeUrl, 'web3.js');
     }
 
     
     async deploy(contract) {
-        const deploymentArguments = Array.prototype.slice.call(arguments) 
-        deploymentArguments.splice(0, 1)
+        const deploymentArguments = Array.prototype.slice.call(arguments);
+        deploymentArguments.splice(0, 1);
     
-        await this._preValidateArguments(contract, deploymentArguments) 
+        await this._preValidateArguments(contract, deploymentArguments);
         
         /* Create a SimpleProject to interact with ZeppelinOS programmatically. */
-        const project = await this._createProject()
-        const contractToDeploy = await this._readContract(contract)
+        const project = await this._instantiateProject();
+        const contractToDeploy = await this._readContract(contract);
    
         let proxyInstance;
-        // ToDo rewrite this AI to a smarter code :))))
-        if(!fs.existsSync('./proxy.json')) {
-            proxyInstance = await this._createProxy(project, contractToDeploy, deploymentArguments)
-        } else {
-            let createdProxy = JSON.parse(fs.readFileSync('./proxy.json'))
-            createdProxy = createdProxy[contractToDeploy.schema.contractName]
-            if(createdProxy && createdProxy.network === this.nodeUrl) {
-                proxyInstance = await this._updateProxy(project, contractToDeploy, deploymentArguments)
+        let proxyInstanceData = await this._findProxyInstanceData(contractToDeploy); // returns proxy data if it was already created
+
+        try {
+            // if proxy was deployed on the same network it will only be updated; otherwise a new instance will be created
+            if(proxyInstanceData && proxyInstanceData.network === this.nodeUrl) {
+                proxyInstance = await this._updateProxy(project, contractToDeploy, deploymentArguments);
             } else {
-                proxyInstance = await this._createProxy(project, contractToDeploy, deploymentArguments)
+                proxyInstance = await this._createProxy(project, contractToDeploy, deploymentArguments);
             }
 
-        }
+            proxyInstance = await this.wrapDeployedContract(contract, proxyInstance.address);
 
-        try{
-            proxyInstance = await this.wrapDeployedContract(contract, proxyInstance.address)
         } catch (e) {
-            let { transaction, transactionReceipt } = await this._prepareTransactionStatus(project)
-            await this._postValidateTransaction(contract, transaction, transactionReceipt)
+            console.log("e", e)
+            // if a transaction failed we need to customize params for the table result
+            let { transaction, transactionReceipt } = await this._prepareTransactionStatus(project);
+            await this._postValidateTransaction(contract, transaction, transactionReceipt);
         }
         
-        await this._logAction(this.constructor.name, contract.contractName, 'Not provided', 0, project.txParams.gasPrice,'Not provided', proxyInstance.contractAddress) 
-        return proxyInstance
+        await this._logAction(this.constructor.name, contract.contractName, 'Not provided', 0, project.txParams.gasPrice,'Not provided', proxyInstance.contractAddress);
+        return proxyInstance;
     }
+
+    async _instantiateProject() {
+        let projectName = path.parse(process.cwd()).name; // the name of the project is needed to instantiate it
+        let transactionParams = await this._setTransactionParams();
+        return new SimpleProject(projectName, undefined, transactionParams);
+    }
+
 
     async _readContract(contract) {
-        await Contracts.setLocalBuildDir(`${process.cwd()}/build`)
-        return Contracts.getFromLocal(contract.contractName)
+        await Contracts.setLocalBuildDir(`${process.cwd()}/build`);
+        return Contracts.getFromLocal(contract.contractName);
     }
 
 
-    async _createProject() {
-        let projectName = path.parse(process.cwd()).name // the name of the project is needed to instantiate it
-        let transactionParams = await this._setTransactionParams()
-        return new SimpleProject(projectName, transactionParams)
+    async _findProxyInstanceData(contractToDeploy) {
+        if(!fs.existsSync('./proxy.json')) {
+            return
+        }
+
+        let deployedProxyData = JSON.parse(fs.readFileSync('./proxy.json'));
+        return deployedProxyData[contractToDeploy.schema.contractName];
     }
 
 
     async _updateProxy(project, contractToDeploy, deploymentArguments) {
-        let proxy = JSON.parse(fs.readFileSync('./proxy.json'))
-        let proxyAddress = proxy[contractToDeploy.schema.contractName].address 
-        let methodName = deploymentArguments[0]  // the name of the initMethod (if any)
-        deploymentArguments.splice(0, 1) //init arguments
-        let proxyInstance = await project.upgradeProxy(proxyAddress, contractToDeploy, { initMethod: methodName, initArgs: [deploymentArguments] })
-        return proxyInstance 
+        let proxy = JSON.parse(fs.readFileSync('./proxy.json'));
+        let proxyAddress = proxy[contractToDeploy.schema.contractName].address;
+        let methodName = deploymentArguments[0];  // the name of the initMethod (if any)
+        deploymentArguments.splice(0, 1); //init arguments
+        let proxyInstance = await project.upgradeProxy(proxyAddress, contractToDeploy, { initMethod: methodName, initArgs: [deploymentArguments] });
+        return proxyInstance;
     }
 
 
     async _createProxy(project, contractToDeploy, deploymentArguments) {
-        let proxyInstance = await project.createProxy(contractToDeploy, { initArgs: deploymentArguments })
+        let proxyInstance = await project.createProxy(contractToDeploy, { initArgs: deploymentArguments });
         let readContract = {};
+
+        let proxyData = fs.existsSync('./proxy.json')? JSON.parse(fs.readFileSync('./proxy.json')) : {};
+        proxyData[proxyInstance.schema.contractName] = {address: proxyInstance.options.address, deployer: proxyInstance.options.from, network: proxyInstance.currentProvider.host};
         
-        if(fs.existsSync('./proxy.json')) {
-            readContract = JSON.parse(fs.readFileSync('./proxy.json'));
-            readContract[proxyInstance.schema.contractName] = {address: proxyInstance.options.address, deployer: proxyInstance.options.from, network: proxyInstance.currentProvider.host}
-        } else {
-            readContract[proxyInstance.schema.contractName] = {address: proxyInstance.options.address, deployer: proxyInstance.options.from, network: proxyInstance.currentProvider.host}
-        } 
-        
-        fs.writeFileSync('./proxy.json', JSON.stringify(readContract, null, 4)) // second param is replacer if needed, third is number of spaces
-        return proxyInstance
+        fs.writeFileSync('./proxy.json', JSON.stringify(proxyData, null, 4)); // second param is replacer if needed, third is number of spaces
+        return proxyInstance;
     }
 
 
     async _setTransactionParams() {
-        let txParams = {} 
-        let wallet = await ZWeb3.eth().accounts.wallet.add(this.signer.privateKey)
-        txParams.from = wallet.address
-        txParams = await this._overrideDeployTransactionConfig(txParams)
-        txParams.gas = txParams.gasLimit // Zos deployer requires gas property, not gasLimit
-        Contracts.artifactDefaults = txParams
-		return txParams 
+        let txParams = {};
+        let wallet = await ZWeb3.eth().accounts.wallet.add(this.signer.privateKey);
+        txParams.from = wallet.address;
+        txParams = await this._overrideDeployTransactionConfig(txParams);
+        txParams.gas = txParams.gasLimit; // Zos deployer requires gas property, not gasLimit
+        Contracts.artifactDefaults = txParams; // set txParams on contract too
+		return txParams;
     }
 
     async _prepareTransactionStatus(project) {
-        let transaction = {hash: "Not provided", gasPrice: project.txParams.gasPrice}
-        let transactionReceipt = {hash: "Not provided", status: 0, gasUsed: "Not Provided"}
-        return { transaction, transactionReceipt }
+        let transaction = {hash: "Not provided", gasPrice: project.txParams.gasPrice};
+        let transactionReceipt = {hash: "Not provided", transactionHash: "", status: 0, gasUsed: "Not Provided"};
+        return { transaction, transactionReceipt };
     }
  
 }
--- test/deployer/zos-deployer/zos-jsonrpc-private-key-deployer.js
+++ test/deployer/zos-deployer/zos-jsonrpc-private-key-deployer.js
@@ -18,12 +18,14 @@ const defaultConfigs = {
 	gasLimit: config.defaultGasLimit
 }
 
-let contractInstance;
+let zosContractInstance;
+let limeContractInstance;
 let proxyInstance;
 let deployer;
 let signer;
 let notAdmin;
 let ZosContract;
+let ZosContractInitial;
 let LimeFactory;
 let currentDir;
 
@@ -36,99 +38,97 @@ describe('Zos deployer tests', async () => {
 			currentDir = process.cwd();
 			process.chdir('./test/deployer/zos-deployer')
 			await compiler.run('.')
+			LimeFactory = JSON.parse(fs.readFileSync('./build/LimeFactory.json'));
 			ZosContract = JSON.parse(fs.readFileSync('./build/ZosContract.json'));
 			deployer = new etherlime.ZosJSONRPCPrivateKeyDeployer(config.randomPrivateKey, config.nodeUrl, defaultConfigs);
 			signer = deployer.signer
 			notAdmin = new ethers.Wallet(ganacheSetupConfig.accounts[5].secretKey, deployer.provider);
 		})
+
+		it('should deploy new contract without init method correctly', async () => {
+			limeContractInstance = await deployer.deploy(LimeFactory);
+			
+			assert.ok(isAddress(limeContractInstance.contractAddress), 'The deployed address is incorrect');
+			assert.deepEqual(signer.address, limeContractInstance.signer.address, "The stored signer does not match the inputted one");
+			assert.deepEqual(config.nodeUrl, limeContractInstance.provider.connection.url, "The stored provider does not match the inputted one");
+		})
 	
 		it('should deploy contract with init method correctly', async () => {
-			contractInstance = await deployer.deploy(ZosContract, 10);
+			zosContractInstance = await deployer.deploy(ZosContract, 10);
 			
-			assert.ok(isAddress(contractInstance.contractAddress), 'The deployed address is incorrect');
-			assert.deepEqual(signer.address, contractInstance.signer.address, "The stored signer does not match the inputted one");
-			assert.deepEqual(config.nodeUrl, contractInstance.provider.connection.url, "The stored provider does not match the inputted one");
+			assert.ok(isAddress(zosContractInstance.contractAddress), 'The deployed address is incorrect');
+			assert.deepEqual(signer.address, zosContractInstance.signer.address, "The stored signer does not match the inputted one");
+			assert.deepEqual(config.nodeUrl, zosContractInstance.provider.connection.url, "The stored provider does not match the inputted one");
 	
 		});
 
-		it('should have json file created with proxy instance data', async () => {
+		it('should have json file created with proxy instance data of all deployed contracts', async () => {
 			assert.ok(fs.existsSync('./proxy.json'))
 			proxyInstance = JSON.parse(fs.readFileSync('./proxy.json'))
-			let proxyAddress = proxyInstance[ZosContract.contractName].address
-			assert.deepEqual(proxyAddress, contractInstance.contractAddress)
+
+			let limeProxyAddress = proxyInstance[LimeFactory.contractName].address
+			assert.deepEqual(limeProxyAddress, limeContractInstance.contractAddress)
+
+			let zosProxyAddress = proxyInstance[ZosContract.contractName].address
+			assert.deepEqual(zosProxyAddress, zosContractInstance.contractAddress)
+
+			
 		})
 	
 		it('should read value from the contract', async () => {
-			let instance = await contractInstance.contract.connect(notAdmin)
+			let instance = await zosContractInstance.contract.connect(notAdmin)
 			let value = await instance.num()
 			assert.equal(value.toNumber(), 10)
 		})
 	
 		it('should not find a specific function before upgrading', async () => {
 			const throwingFunction = () => {
-				contractInstance.newFunction()
+				zosContractInstance.newFunction()
 			}
-			assert.throws(throwingFunction, "contractInstance.newFunction is not a function", "The contract not throw if specific function is called before upgrading");
+			assert.throws(throwingFunction, "zosContractInstance.newFunction is not a function", "The contract not throw if specific function is called before upgrading");
 		})
 	
-		it('should update contract correctly', async () => {
-			let zosContractInitial = fs.readFileSync('./contracts/ZosContract.sol')
+		it('should update contract correctly without changing its address', async () => {
+			ZosContractInitial = fs.readFileSync('./contracts/ZosContract.sol')
 			fs.writeFileSync('./contracts/ZosContract.sol', upgradedContract)
 			fs.removeSync('./build')
 			await compiler.run('.')
 			ZosContract = JSON.parse(fs.readFileSync('./build/ZosContract.json'));
-			let contractAddressBeforeUpgrading = contractInstance.contractAddress;
-			contractInstance = await deployer.deploy(ZosContract);
-			assert.ok(isAddress(contractInstance.contractAddress), 'The deployed address is incorrect');
-			assert.equal(contractInstance.contractAddress, contractAddressBeforeUpgrading)
-			fs.writeFileSync('./contracts/ZosContract.sol', zosContractInitial)
+			let contractAddressBeforeUpgrading = zosContractInstance.contractAddress;
+
+			zosContractInstance = await deployer.deploy(ZosContract);
+
+			assert.ok(isAddress(zosContractInstance.contractAddress), 'The deployed address is incorrect');
+			assert.equal(zosContractInstance.contractAddress, contractAddressBeforeUpgrading)
 		})
 	
-		// it('should execute a specific function after upgrading', async () => {
-		// 	let instance = await contractInstance.contract.connect(notAdmin)
-		// 	// await instance.newFunction(5)
-		// 	let value = await instance.num()
-		// 	assert.equal(value.toNumber(), 15, "The value is not updated after newFunction execution")
-		// })
+		it('should have a specific function after upgrading', async () => {
+			assert.equal(typeof zosContractInstance.newFunction, 'function')
+		})
 
-		it('should add new proxy to proxy.json', async () => {
-			LimeFactory = JSON.parse(fs.readFileSync('./build/LimeFactory.json'));
-			contractInstance = await deployer.deploy(LimeFactory);
-			
-			assert.ok(isAddress(contractInstance.contractAddress), 'The deployed address is incorrect');
-			assert.deepEqual(signer.address, contractInstance.signer.address, "The stored signer does not match the inputted one");
-			assert.deepEqual(config.nodeUrl, contractInstance.provider.connection.url, "The stored provider does not match the inputted one");
 
+		it('should deploy on ropsten test net', async () => {
+			deployer = new etherlime.ZosJSONRPCPrivateKeyDeployer(config.infuraRopstenPrivateKey, config.infuraRopstenURL, defaultConfigs);
+			limeContractInstance = await deployer.deploy(LimeFactory)
+			assert.ok(isAddress(limeContractInstance.contractAddress), 'The deployed address is incorrect');
+		})
+
+		it('should update the network and contract address in json file', async () => {
 			proxyInstance = JSON.parse(fs.readFileSync('./proxy.json'))
-			let proxyAddress = proxyInstance[LimeFactory.contractName].address
-			assert.deepEqual(proxyAddress, contractInstance.contractAddress)
-	
+
+			let limeProxyAddress = proxyInstance[LimeFactory.contractName].address
+			let network = proxyInstance[LimeFactory.contractName].network
+			assert.deepEqual(limeProxyAddress, limeContractInstance.contractAddress)
+			assert.equal(network, config.infuraRopstenURL)
 		})
 
-		it.only('should deploy on ropsten test net', async () => {
-			LimeFactory = JSON.parse(fs.readFileSync('./build/LimeFactory.json'));
-			let walletPK = '9C854BD14857DD1F107ECB94E1AEEAA8E9538E3DC41D082D5FE0E452E87A2F28'
-			let infuraURL = 'https://ropsten.infura.io/ede61953adb34beeb5106a2c0c61f200'
-			deployer = new etherlime.ZosJSONRPCPrivateKeyDeployer(walletPK, infuraURL, defaultConfigs);
-			contractInstance = await deployer.deploy(LimeFactory)
-			console.log("contractinst", contractInstance.contractAddress)
+
+		it('should throw if transaction failed', async () => {
+			await assert.isRejected(deployer.deploy(ZosContract, "param to fail"), "failed");
 		})
-
-		// it('should override data in json file if we want to deploy on different network', async() => {
-		// 	deployer = new etherlime.ZosJSONRPCPrivateKeyDeployer(config.randomPrivateKey, config.alternativeNodeUrl, defaultConfigs);
-		// 	let addressBefore = contractInstance.contractAddress
-		// 	contractInstance = await deployer.deploy(LimeFactory);
-		// 	proxyInstance = JSON.parse(fs.readFileSync('./proxy.json'))
-		// 	let proxyAddress = proxyInstance[LimeFactory.contractName].address
-		// 	assert.notDeepEqual(proxyAddress, addressBefore)
-		// })
-
-
-		// it('should return undefined contract instance if transaction failed', async () => {
-		// 	await assert.isRejected(deployer.deploy(ZosContractUpgraded, "param to fail"), "failed");
-		// })
 
 		after(async() => {
+			fs.writeFileSync('./contracts/ZosContract.sol', ZosContractInitial)
 			fs.removeSync('./proxy.json')
 			fs.removeSync('./build')
 			process.chdir(currentDir)
