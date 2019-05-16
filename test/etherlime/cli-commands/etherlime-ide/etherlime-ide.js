21b6753b9e4620344c4f659bdfda1d88706f3fb7
Desimira <desimira@limechain.tech>
2019-05-16 15:13:07 +0300

    ide tests fix

--- test/etherlime/cli-commands/etherlime-ide/etherlime-ide.js
+++ test/etherlime/cli-commands/etherlime-ide/etherlime-ide.js
@@ -19,19 +19,19 @@ describe('etherlime-ide cli command', () => {
         killProcessByPID(childProcess.process.pid)
     })
 
-    // it('should update ide repo if it was already cloned', async () => {
-    //     let expectedOutput = "Running IDE"
-    //     let childProcess = await runCmdHandler(`etherlime ide`, expectedOutput);
-    //     assert.include(childProcess.output, expectedOutput)
-    //     killProcessByPID(childProcess.process.pid)
-    // })
+    it('should update ide repo if it was already cloned', async () => {
+        let expectedOutput = "Running IDE"
+        let childProcess = await runCmdHandler(`etherlime ide`, expectedOutput);
+        assert.include(childProcess.output, expectedOutput)
+        killProcessByPID(childProcess.process.pid)
+    })
 
-    // it('should run etherlime ide on specific port', async () => {
-    //     let expectedOutput = "Starting solc server"
-    //     let childProcess = await runCmdHandler(`etherlime ide --port=5000`, expectedOutput);
-    //     assert.include(childProcess.output, expectedOutput)
-    //     killProcessByPID(childProcess.process.pid)
-    // })
+    it('should run etherlime ide on specific port', async () => {
+        let expectedOutput = "Starting solc server"
+        let childProcess = await runCmdHandler(`etherlime ide --port=5000`, expectedOutput);
+        assert.include(childProcess.output, expectedOutput)
+        killProcessByPID(childProcess.process.pid)
+    })
 
     after(async function () {
         fs.removeSync('./contracts');
