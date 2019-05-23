5ced3c01074f3693bdb237ee26756dff7ccc9e47
Ognyan Chikov <o.chikov@propy.com>
2019-05-23 16:55:27 +0300

    leave comment

--- test/etherlime-lib/deployer/deployer.js
+++ test/etherlime-lib/deployer/deployer.js
@@ -7,7 +7,7 @@ const assert = require('assert');
 const store = require('./../../../packages/etherlime-logger/logs-store/logs-store');
 const sinon = require('sinon');
 const fs = require('fs-extra');
-var axios = require('../../../packages/etherlime/node_modules/axios');
+var axios = require('../../../packages/etherlime/node_modules/axios'); // I am loading axios like that, because MOCK Adapter is mocking another axios instance and not the instance that is used in the Verifier!
 var MockAdapter = require('axios-mock-adapter');
 
 const isAddress = require('./../../../packages/etherlime-utils/utils/address-utils').isAddress;
